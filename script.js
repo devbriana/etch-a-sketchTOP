2dec3e674f611e6751325e22e3f769c9e6eba80a
devbriana <dev.brianam@gmail.com>
2025-04-15 19:05:46 -0400

    Add mouseover event listeners and color buttons
    - Added three different button options for user to change square colors
      from solid black, to greyscale, to multicolored,
    - Added "click" event listeners for each button.
    - Each button event listener called its own function named
      "changeDivColor...".
    - Each "changeDivColor..." function would update a variable called
      currentMode which related to which button was clicked.
    - Each "changeDivColor..." function also added a mouseover event
      listener which called the function updateSquares.
    - updateSquares included an if..else if block which would update the
      div background color of the squares when moused over based on what the
      currentMode was updated to (which was based on what button was last
      clicked).
    - For the grey currentMode in updateSquares I changed the opacity levels
      with each hover.
    - Needs update for multicolored currentMode in updateSquares by
      randomizing the square colors with each hover.
    - Needs refactoring and organization.
    - Needs README updated.
    - Everything currently functioning properly.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var divContainer = document.getElementById('container');
 var dimensionsDisplay = document.getElementById('dimensions-display')
-var btn = document.querySelector('button');
+// buttons
+var pixelsBtn = document.getElementById('pixels-btn');
+var solidBtn = document.getElementById('solid-btn');
+var greyScaleBtn = document.getElementById('grey-scale-btn')
+var multicolorBtn = document.getElementById('multicolor-btn');
 
+var currentMode;
 
 
 var dimensions = 16;
@@ -9,7 +14,7 @@ var size;
 var divCount = 0;
 
 //button click to call getDimensions
-btn.addEventListener("click", getDimensions)
+pixelsBtn.addEventListener("click", getDimensions)
 
 // ask user for how many squares they want per side
 function getDimensions(e) {
@@ -64,15 +69,82 @@ function createDivs(divCount) {
     while (i < divCount) {
         const newDiv = document.createElement('div');
         newDiv.className = "new-div";
+        const allSquares = document.querySelectorAll(".new-div");
         divContainer.appendChild(newDiv);
         newDiv.style.height = size;
         newDiv.style.width = size;
         console.log(i);
+        // set up hover event listener
+        solidBtn.addEventListener("click", changeDivColorSolid);
+        greyScaleBtn.addEventListener("click", changeDivColorGrey);
+        multicolorBtn.addEventListener("click", changeDivColorMulti);
+
+        //solid divs
+        function changeDivColorSolid() {
+            /*allSquares.forEach((square) => {
+                newDiv.style.backgroundColor = "white";
+                newDiv.style.opacity = 1;
+            })*/
+            currentMode = "solid";
+            console.log("solid button clicked");
+            console.log(currentMode);
+            newDiv.addEventListener("mouseover", updateSquares);
+        }
+        
+
+        // grey divs
+        function changeDivColorGrey() {
+            allSquares.forEach((square) => {
+                //newDiv.style.backgroundColor = "white";
+                //newDiv.style.opacity = 0;
+            })
+            currentMode = "grey";
+
+            console.log("grey button clicked");
+            console.log(currentMode);
+            newDiv.addEventListener("mouseover", updateSquares);
+
+        }
+      
+
+        // multicolored divs
+        function changeDivColorMulti() {
+            /*allSquares.forEach((square) => {
+                newDiv.style.backgroundColor = "white";
+                newDiv.style.opacity = 1;
+            })*/
+            currentMode = "multi";
+            console.log("color button clicked");
+            console.log(currentMode);
+            newDiv.addEventListener("mouseover", updateSquares);
+        }
+       
+        function updateSquares() {
+            if (currentMode === "solid") {
+                newDiv.style.opacity = 1;
+                newDiv.style.backgroundColor = "black";
+            }
+            else if (currentMode === "grey") {
+                let currentOpacity = parseFloat(newDiv.style.opacity) || 0; // start at 0 if not set
+                newDiv.style.backgroundColor = "rgba(76, 77, 79)";
+            
+                if (currentOpacity < 1) {
+                    currentOpacity += 0.2;
+                }
+                newDiv.style.opacity = currentOpacity;
+            } else if (currentMode === "multi") {
+                newDiv.style.opacity = 1;
+                newDiv.style.backgroundColor = "pink";
+
+            }
+          
+
+        }
 
         i++;
         }
+
+   
 }
 
 createDivs(divCount);
-
-
